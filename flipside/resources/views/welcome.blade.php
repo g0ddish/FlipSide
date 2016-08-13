874d8dfc6fb30773413ede198b80f1e9d8014241
Alex Hughes <alex@solutionblender.ca>
2016-08-12 20:32:59 -0400

    go

---
+++ flipside/resources/views/welcome.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>The Flipside</title>
+
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Mountains+of+Christmas" />
+        <link href="./css/bootstrap.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <style>
+            html, body {
+                height: 100%;
+                background-image: url("./img/footer_lodyas.png");
+
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Mountains of Christmas';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: top;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 45px;
+               color:green;
+            }
+
+            .title-2 {
+                    font-size: 25px;
+                    color:red;
+            }
+
+            .youtube{
+                position: relative;
+                padding-bottom: 56.25%; /* 16:9 */
+                height: 0;
+                margin-top:-45px;
+            }
+
+            .youtube iframe {
+                margin: auto;
+                position: absolute;
+                top: 0; left: 0; bottom: 0; right: 0;
+                width: 75%;
+                height: 75%;
+            }
+
+            hr.style17 {
+                width:75%;
+                border-top: 1px solid darkgray;
+                text-align: center;
+            }
+            hr.style17:after {
+                font-family: "Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif;
+                content: '\00a7';
+                display: inline-block;
+                position: relative;
+                top: -14px;
+                padding: 0 10px;
+                color: #8c8b8b;
+                font-size: 18px;
+                -webkit-transform: rotate(60deg);
+                -moz-transform: rotate(60deg);
+                transform: rotate(60deg);
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">The Flipside </div>
+                <div class="title-2">Slutty Clause 3</div>
+
+        </div>
+            <div class="youtube">
+                <iframe  src="https://www.youtube.com/embed/HgVCArO5eTo?autoplay=1" frameborder="0" allowfullscreen></iframe>
+            </div>
+
+            <div class="footer">
+                <hr class="style17">
+                <div class="col-md-6 col-md-offset-3" style="">
+                    <div class="panel panel-default" style="background: black; border-color: gray">
+                        <div class="panel-body" style="color:white">
+                            <img style=" padding-left:25%;" class="img-responsive" src="./img/tSp5J0P.gif" />
+                            <h1>Get in contact to work with us!</h1>
+                            <h2>647-883-4343</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
